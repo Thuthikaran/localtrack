b13b665948294a51d8cfdc72fa0b7211bd270157
Thuthikaran Easvaran <thuthikaraneasvaran@gmail.com>
2025-01-09 13:41:00 +0100

    commiting all changes

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // Selectors 
+const input = document.querySelector('#habit');
+const habitForm = document.querySelector('#habit-form');
+
+function retrieve (e) {
+    e.preventDefault();  // Prevents the form from reloading the page (default behavior of form submission).
+    let newItem = input.value.trim();  // Takes the current value from the input field and stores it in `newItem`.
+    console.log(newItem);  // Logs the input value to the console.
+};
+
+
+habitForm.addEventListener('submit', retrieve);
